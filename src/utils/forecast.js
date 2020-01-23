d49ec23ba85104332970c924f2f1f23aa96cc282
Alejandro Lozano <alozano.m@hotmail.com>
2020-01-23 13:24:36 +0100

    Minor changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,4 @@
-const request = require('request')
+const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/ce5d42840497a81bd46896025de339d8/' + latitude + ',' + longitude + '?lang=es&units=si';
@@ -9,9 +9,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. The uvIndex is ' + body.currently.uvIndex);
         }
     })
-}
+};
 
-module.exports = forecast
+module.exports = forecast;
